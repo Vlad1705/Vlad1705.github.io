22c436039fee55b9861025f26b8606a0e83540ff
vlad1705 <43314270+vlad1705@users.noreply.github.com>
2020-05-12 19:15:43 +0300

    Add files via upload

---
+++ healthy_gulp/js/supplements.js
@@ -0,0 +1,50 @@
+API_URL = 'http://2abb90d4.ngrok.io/api/'
+API_URL1 = 'http://2abb90d4.ngrok.io/search/query?q='
+
+
+
+document.getElementById('current_supplement_submit').onclick = function(){
+    let elem = document.getElementById("current_supplement").value
+    
+    let request = new XMLHttpRequest()
+    request.open("GET", API_URL1 + elem, false);
+    request.send();
+    let data = JSON.parse(request.responseText)
+    
+    showDetails(data[0])
+    }
+
+
+function showDetails(supplement) {
+    let request = new XMLHttpRequest()
+    request.open("GET", API_URL + "classes/" + supplement['class_id'], false);
+    request.send();
+    let data = JSON.parse(request.responseText)
+    console.log(data)
+    let class_name = data['name']
+    request = new XMLHttpRequest()
+    request.open("GET", API_URL + "purposes/" + supplement['purpose_id'], false);
+    request.send();
+    data = JSON.parse(request.responseText)
+    console.log(data)
+    let purpose_name = data['name']
+    request = new XMLHttpRequest()
+    request.open("GET", API_URL + "supplements/" + supplement['id'] + "/side-effects", false);
+    request.send();
+    data = JSON.parse(request.responseText)
+    console.log(data)
+    let side_effects = []
+    for (let i = 0; i< data.length; i++) {
+        side_effects.push(data[i]['name'])
+    }
+    document.getElementById('supplement-class').innerText = ('About - ' + class_name)
+    document.getElementById('supplement-purpose').innerText = ("Purpose - " + purpose_name)
+    document.getElementById('supplement-side-effects').innerText = ("Side-effects: " + side_effects.join(', '))
+}
+
+function resetColors() {
+    let children = document.getElementById('supplement-container').children
+    for (let i = 0; i < children.length; i++) {
+        children[i].style.backgroundImage = 'linear-gradient(to right, #4f8aff, white)'
+    }
+}
